2129099933e910010444268fa8c816f90cd32fbd
Mikhaylov-yv <dezmond22@yandex.ru>
2020-01-21 00:16:32 +0300

    Убрал какой-то код из выдачи

--- python_script.py
+++ python_script.py
@@ -2,13 +2,17 @@ import sys
 import os.path
 
 cache_fill_name = 'cache.csv'
+csv_delimiter = ','
 print(sys.argv)
 delimiter1 = sys.argv.index(';')
 print(delimiter1)
 text_in = ' '.join(sys.argv[1:delimiter1])
 delimiter2 = sys.argv.index(';',delimiter1+1)
-print(delimiter2)
-text_out = ' '.join(sys.argv[delimiter2+1:-4])
+delimiter3 = -3
+if len(sys.argv[delimiter3-1]) >= 32:
+	delimiter3 = delimiter3 -1
+print(delimiter2) 
+text_out = ' '.join(sys.argv[delimiter2+1:delimiter3])
 print(text_in, '\n', text_out)
 lang_in = sys.argv[delimiter1+1]
 lang_out = sys.argv[-1]
@@ -17,7 +21,7 @@ cache_fill_list = [text_in,text_out,lang_in,lang_out]
 def update_fill(fill_name,translet_list):
 	fill = open(fill_name, 'a')
 	print(translet_list)
-	string = ';'.join(translet_list)
+	string = csv_delimiter.join(translet_list)
 	fill.write(string + '\n')
 	fill.close()
 
@@ -27,4 +31,3 @@ if os.path.exists(cache_fill_name):
 	print('Есть')
 	update_fill(cache_fill_name,cache_fill_list)
 else: f = open(cache_fill_name, 'w')
-print('test')
